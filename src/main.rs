92918c984e852398bacad9907173ca8c68f551aa
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-02-13 00:11:06 -0500

    Added a different specification for safety

--- src/main.rs
+++ src/main.rs
@@ -40,18 +40,22 @@ verus! {
         ||| exists |book: Book, patron_name: String| check_out(library, book, patron_name, library_prime)
     }
 
-    // Now, we will write the actual safety specification
-    spec fn safety(library: Library) -> bool {
+    // We will now write this specification a little differently.
+    spec fn has_at_most_one_book(library: Library, patron_name: String) -> bool {
         forall |book1: Book, book2: Book| #![auto]
             library.dom().contains(book1) &&
             library.dom().contains(book2) &&
-            library.index(book1) != Card::Shelf &&
-            book1 != book2
-            ==> library.index(book1) != library.index(book2)
+            library.index(book1) == Card::patron(patron_name) &&
+            library.index(book2) == Card::patron(patron_name)
+            ==> book1 == book2
     }
-    // This just happens to be a very strong specification, such that even without writing any explict proof,
-    // the verus is able to prove our proof on the system.
 
+    spec fn safety(library: Library) -> bool {
+        forall |patron_name: String| #![auto] has_at_most_one_book(library, patron_name)
+    }
+
+    // Now, even for a correct specification, verus is not able to prove that our system follows
+    // the provided proof. Therefore, we need to add hints/triggers to help verus.
     proof fn ensures_safety()
         ensures
         forall |library: Library| init(library) ==> safety(library),
