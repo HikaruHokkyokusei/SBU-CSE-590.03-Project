02ee15e5b586947fcbf32211b06dfd07a0f6676e
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-02-13 00:37:12 -0500

    Added proof to prove the specification for the system

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,6 @@ verus! {
         ||| exists |book: Book, patron_name: String| check_out(library, book, patron_name, library_prime)
     }
 
-    // We will now write this specification a little differently.
     spec fn has_at_most_one_book(library: Library, patron_name: String) -> bool {
         forall |book1: Book, book2: Book| #![auto]
             library.dom().contains(book1) &&
@@ -51,16 +50,33 @@ verus! {
     }
 
     spec fn safety(library: Library) -> bool {
-        forall |patron_name: String| #![auto] has_at_most_one_book(library, patron_name)
+        forall |patron_name: String| has_at_most_one_book(library, patron_name)
     }
 
-    // Now, even for a correct specification, verus is not able to prove that our system follows
-    // the provided proof. Therefore, we need to add hints/triggers to help verus.
     proof fn ensures_safety()
         ensures
         forall |library: Library| init(library) ==> safety(library),
         forall |library: Library, library_prime: Library| safety(library) && next(library, library_prime) ==> safety(library_prime)
-    {}
+    {
+        assert forall |library: Library, library_prime: Library| safety(library) && next(library, library_prime) implies safety(library_prime) by {
+            assume(safety(library));
+            assume(next(library, library_prime));
+
+            assert forall |patron2: String| has_at_most_one_book(library_prime, patron2) by {
+                if (exists |book: Book, patron1: String| check_out(library, book, patron1, library_prime)) {
+                    let (book, patron1) = choose |book: Book, patron1: String| check_out(library, book, patron1, library_prime);
+                    if (patron2 == patron1) {
+                        assert(has_at_most_one_book(library_prime, patron2));
+                    } else {
+                        assert(has_at_most_one_book(library, patron2));
+                        assert(has_at_most_one_book(library_prime, patron1));
+                    }
+                } else {
+                    assert(has_at_most_one_book(library, patron2));
+                }
+            }
+        }
+    }
 
     fn main() { }
 }
