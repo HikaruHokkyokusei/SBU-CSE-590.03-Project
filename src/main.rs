037f43ec8036c1f155983fb6cd5ae523f4ee0c5b
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-02-12 21:55:24 -0500

    Defined the basic state of the state machine

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 use vstd::prelude::*;
 
 verus! {
+    // Consider we have a Library.
+    // In our library, we have books, and each book has a card associated with it.
+    // The card tells whether the book is placed in Shelf or borrowed by a Patron.
+
+    // Our Library has a criteria that no two books can be borrowed by the same Patron.
+
+    // Below, we define the Library, Book and Card.
+    struct Book {
+        title: String
+    }
+
+    enum Card {
+        Shelf,
+        Patron { name: String }
+    }
+
+    type Library = Map<Book, Card>;
+
+    // If we talk in terms of a state machine, the Library if our state.
+
     fn main() { }
 }
