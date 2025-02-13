4a32b993f0c2e7a0dbe906a0ac8ca4343d367f1f
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-02-12 22:43:41 -0500

    Added actions on the state machine

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,50 @@ verus! {
         Patron { name: String }
     }
 
+    impl Card {
+        spec fn patron(name: String) -> Self {
+            Self::Patron { name }
+        }
+    }
+
     type Library = Map<Book, Card>;
 
     // If we talk in terms of a state machine, the Library if our state.
+
+    // We will now specify the initial state of our state machine.
+    spec fn init(library: Library) -> bool {
+        forall |book: Book| #![auto] library.dom().contains(book) ==> library.index(book) == Card::Shelf
+    }
+
+    // We will now define two actions on our state machine.
+
+    // 1. Check Out
+    spec fn check_out(library: Library, book: Book, patron_name: String, library_prime: Library) -> bool {
+        // Enabling Condition 1
+        &&& library.dom().contains(book)
+        // Enabling Condition 2
+        &&& library.index(book) == Card::Shelf
+        // Enabling Condition 3
+        &&& forall |book: Book| #![auto] library.dom().contains(book) ==> library.index(book) != Card::patron(patron_name)
+        // Update
+        &&& library_prime == library.insert(book, Card::patron(patron_name))
+    }
+
+    // 2. Check In
+    spec fn check_in(library: Library, book: Book, patron_name: String, library_prime: Library) -> bool {
+        // Enabling Condition 1
+        &&& library.dom().contains(book)
+        // Enabling Condition 2
+        &&& library.index(book) == Card::patron(patron_name)
+        // Update
+        &&& library_prime == library.insert(book, Card::Shelf)
+    }
+
+    // Finally, we will define next, which specifies whether the new state is a valid next state of the old state
+    spec fn next(library: Library, library_prime: Library) -> bool {
+        ||| exists |book: Book, patron_name: String| check_in(library, book, patron_name, library_prime)
+        ||| exists |book: Book, patron_name: String| check_out(library, book, patron_name, library_prime)
+    }
 
     fn main() { }
 }
