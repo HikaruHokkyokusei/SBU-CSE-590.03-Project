08a69331327b16354348afeb1f2cfe758329b449
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-02-23 17:29:41 -0500

    Defined the state of the system and the initial state specification

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 use vstd::prelude::*;
 
 verus! {
+    struct Constants {
+        z: i64
+    }
+
+    struct Variables {
+        x: i64,
+        y: i64
+    }
+
+    spec fn init(u: Variables) -> bool {
+        u.x == 0 && u.y == 5
+    }
+
     fn main() { }
 }
