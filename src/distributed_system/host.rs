ac2138af0edc976b735aa79a7a17d0a48ebf4322
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-03-05 21:59:47 -0500

    Added specification to define a well formed system and a system in initial state

--- src/distributed_system/host.rs
+++ src/distributed_system/host.rs
@@ -15,4 +15,22 @@ verus! {
     pub(crate) struct Variables {
         pub(crate) decision: Option<Decision>
     }
+
+    impl Constants {
+        pub(crate) open spec fn well_formed(&self, id: int) -> bool {
+            &&& id >= 0
+            &&& self.id == id
+        }
+    }
+
+    impl Variables {
+        pub(crate) open spec fn well_formed(&self, c: &Constants, id: int) -> bool {
+            &&& c.well_formed(id)
+        }
+    }
+
+    pub(crate) open spec fn init(c: &Constants, u: &Variables, host_id: int) -> bool {
+        &&& u.well_formed(c, host_id)
+        &&& u.decision.is_none()
+    }
 }
--- src/distributed_system/network.rs
+++ src/distributed_system/network.rs
@@ -7,4 +7,21 @@ verus! {
     pub(crate) struct Variables {
         pub(crate) sent_messages: Set<Message>
     }
+
+    impl Constants {
+        pub(crate) open spec fn well_formed(&self) -> bool {
+            true
+        }
+    }
+
+    impl Variables {
+        pub(crate) open spec fn well_formed(&self, c: &Constants) -> bool {
+            c.well_formed()
+        }
+    }
+
+    pub(crate) open spec fn init(c: &Constants, u: &Variables) -> bool {
+        &&& u.well_formed(c)
+        &&& u.sent_messages.is_empty()
+    }
 }
