af5052c0714cb0d2de0e6658f70e108443f82222
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-03-05 21:59:47 -0500

    Added specification to define a well formed system and a system in initial state

--- src/distributed_system/coordinator.rs
+++ src/distributed_system/coordinator.rs
@@ -10,4 +10,23 @@ verus! {
         pub(crate) decision: Option<Decision>,
         pub(crate) votes: Seq<Option<Vote>>
     }
+
+    impl Constants {
+        pub(crate) open spec fn well_formed(&self, num_hosts: int) -> bool {
+            self.num_hosts == num_hosts
+        }
+    }
+
+    impl Variables {
+        pub(crate) open spec fn well_formed(&self, c: &Constants, num_hosts: int) -> bool {
+            &&& c.well_formed(num_hosts)
+            &&& self.votes.len() == num_hosts
+        }
+    }
+
+    pub(crate) open spec fn init(c: &Constants, u: &Variables, num_hosts: int) -> bool {
+        &&& u.well_formed(c, num_hosts)
+        &&& u.decision.is_none()
+        &&& forall |i: int| 0 <= i < u.votes.len() ==> u.votes[i].is_none()
+    }
 }
--- src/distributed_system/mod.rs
+++ src/distributed_system/mod.rs
@@ -28,4 +28,28 @@ verus! {
         pub(crate) hosts: Seq<host::Variables>,
         pub(crate) network: network::Variables,
     }
+
+    impl Constants {
+        pub(crate) open spec fn well_formed(&self) -> bool {
+            &&& self.num_hosts > 0
+            &&& self.hosts.len() == self.num_hosts
+        }
+    }
+
+    impl Variables {
+        pub(crate) open spec fn well_formed(&self, c: &Constants) -> bool {
+            &&& c.well_formed()
+            &&& self.hosts.len() == c.hosts.len()
+            &&& self.coordinator.well_formed(&c.coordinator, c.num_hosts)
+            &&& forall |idx: int| 0 <= idx < self.hosts.len() ==> self.hosts[idx].well_formed(&c.hosts[idx], idx)
+            &&& self.network.well_formed(&c.network)
+        }
+    }
+
+    pub(crate) open spec fn init(c: &Constants, u: &Variables) -> bool {
+        &&& u.well_formed(c)
+        &&& coordinator::init(&c.coordinator, &u.coordinator, c.num_hosts)
+        &&& forall |idx: int| 0 <= idx < u.hosts.len() ==> host::init(&c.hosts[idx], &u.hosts[idx], idx)
+        &&& network::init(&c.network, &u.network)
+    }
 }
