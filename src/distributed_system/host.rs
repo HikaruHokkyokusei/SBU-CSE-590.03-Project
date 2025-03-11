510dce8e6fd6e585643f24848eb871ff5814c442
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-03-11 01:01:56 -0400

    Added transition specifcation for the whole system

--- src/distributed_system/host.rs
+++ src/distributed_system/host.rs
@@ -1,4 +1,4 @@
-use super::Decision;
+use super::{Decision, MessageOps, Message};
 use vstd::prelude::*;
 
 verus! {
@@ -32,5 +32,25 @@ verus! {
     pub(crate) open spec fn init(c: &Constants, u: &Variables, host_id: int) -> bool {
         &&& u.well_formed(c, host_id)
         &&& u.decision.is_none()
+    }
+
+    pub(crate) open spec fn vote(c: &Constants, u: &Variables, v: &Variables, message_ops: MessageOps) -> bool {
+        &&& u.well_formed(c, c.id)
+        &&& message_ops.recv == Some(Message::VoteRequest)
+        &&& v.decision == if (c.vote == Vote::No) { Some(Decision::Abort) } else { u.decision }
+        &&& message_ops.send == Some(Message::Vote { sender: c.id, vote: c.vote })
+        &&& v.well_formed(c, c.id)
+    }
+
+    pub(crate) open spec fn learn_decision(c: &Constants, u: &Variables, v: &Variables, message_ops: MessageOps) -> bool {
+        &&& u.well_formed(c, c.id)
+        &&& if let Some(Message::Decision { decision }) = message_ops.recv { v.decision == Some(decision) } else { false }
+        &&& message_ops.send.is_none()
+        &&& v.well_formed(c, c.id)
+    }
+
+    pub(crate) open spec fn step(c: &Constants, u: &Variables, v: &Variables, message_ops: MessageOps) -> bool {
+        ||| vote(c, u, v, message_ops) 
+        ||| learn_decision(c, u, v, message_ops)
     }
 }
--- src/distributed_system/network.rs
+++ src/distributed_system/network.rs
@@ -1,4 +1,4 @@
-use super::Message;
+use super::{Message, MessageOps};
 use vstd::prelude::*;
 
 verus! {
@@ -23,5 +23,14 @@ verus! {
     pub(crate) open spec fn init(c: &Constants, u: &Variables) -> bool {
         &&& u.well_formed(c)
         &&& u.sent_messages.is_empty()
+    }
+
+    pub(crate) open spec fn step(c: &Constants, u: &Variables, v: &Variables, message_ops: MessageOps) -> bool {
+        &&& u.well_formed(c)
+        &&& v.well_formed(c)
+        // Only allow receipt of messages that have been sent
+        &&& if let Some(message) = message_ops.recv { u.sent_messages.contains(message) } else { true }
+        // Update sent messages with any new message
+        &&& if let Some(message) = message_ops.send { v.sent_messages == u.sent_messages.insert(message) } else { true }
     }
 }
