76eb84e4bcd6dd22f8d6cf392084b0875f0e4681
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-03-19 00:51:44 -0400

    Change `pub(crate)` to `pub`

--- src/distributed_system/host.rs
+++ src/distributed_system/host.rs
@@ -2,39 +2,39 @@ use super::{Decision, MessageOps, Message};
 use vstd::prelude::*;
 
 verus! {
-    pub(crate) enum Vote {
+    pub enum Vote {
         Yes,
         No
     }
 
-    pub(crate) struct Constants {
-        pub(crate) id: int,
-        pub(crate) vote: Vote
+    pub struct Constants {
+        pub id: int,
+        pub vote: Vote
     }
 
-    pub(crate) struct Variables {
-        pub(crate) decision: Option<Decision>
+    pub struct Variables {
+        pub decision: Option<Decision>
     }
 
     impl Constants {
-        pub(crate) open spec fn well_formed(&self, id: int) -> bool {
+        pub open spec fn well_formed(&self, id: int) -> bool {
             &&& id >= 0
             &&& self.id == id
         }
     }
 
     impl Variables {
-        pub(crate) open spec fn well_formed(&self, c: &Constants, id: int) -> bool {
+        pub open spec fn well_formed(&self, c: &Constants, id: int) -> bool {
             &&& c.well_formed(id)
         }
     }
 
-    pub(crate) open spec fn init(c: &Constants, u: &Variables, host_id: int) -> bool {
+    pub open spec fn init(c: &Constants, u: &Variables, host_id: int) -> bool {
         &&& u.well_formed(c, host_id)
         &&& u.decision.is_none()
     }
 
-    pub(crate) open spec fn vote(c: &Constants, u: &Variables, v: &Variables, message_ops: MessageOps) -> bool {
+    pub open spec fn vote(c: &Constants, u: &Variables, v: &Variables, message_ops: MessageOps) -> bool {
         &&& u.well_formed(c, c.id)
         &&& message_ops.recv == Some(Message::VoteRequest)
         &&& v.decision == if (c.vote == Vote::No) { Some(Decision::Abort) } else { u.decision }
@@ -42,14 +42,14 @@ verus! {
         &&& v.well_formed(c, c.id)
     }
 
-    pub(crate) open spec fn learn_decision(c: &Constants, u: &Variables, v: &Variables, message_ops: MessageOps) -> bool {
+    pub open spec fn learn_decision(c: &Constants, u: &Variables, v: &Variables, message_ops: MessageOps) -> bool {
         &&& u.well_formed(c, c.id)
         &&& if let Some(Message::Decision { decision }) = message_ops.recv { v.decision == Some(decision) } else { false }
         &&& message_ops.send.is_none()
         &&& v.well_formed(c, c.id)
     }
 
-    pub(crate) open spec fn step(c: &Constants, u: &Variables, v: &Variables, message_ops: MessageOps) -> bool {
+    pub open spec fn step(c: &Constants, u: &Variables, v: &Variables, message_ops: MessageOps) -> bool {
         ||| vote(c, u, v, message_ops)
         ||| learn_decision(c, u, v, message_ops)
     }
--- src/distributed_system/network.rs
+++ src/distributed_system/network.rs
@@ -2,30 +2,30 @@ use super::{Message, MessageOps};
 use vstd::prelude::*;
 
 verus! {
-    pub(crate) struct Constants {}
+    pub struct Constants {}
 
-    pub(crate) struct Variables {
-        pub(crate) sent_messages: Set<Message>
+    pub struct Variables {
+        pub sent_messages: Set<Message>
     }
 
     impl Constants {
-        pub(crate) open spec fn well_formed(&self) -> bool {
+        pub open spec fn well_formed(&self) -> bool {
             true
         }
     }
 
     impl Variables {
-        pub(crate) open spec fn well_formed(&self, c: &Constants) -> bool {
+        pub open spec fn well_formed(&self, c: &Constants) -> bool {
             c.well_formed()
         }
     }
 
-    pub(crate) open spec fn init(c: &Constants, u: &Variables) -> bool {
+    pub open spec fn init(c: &Constants, u: &Variables) -> bool {
         &&& u.well_formed(c)
         &&& u.sent_messages.is_empty()
     }
 
-    pub(crate) open spec fn step(c: &Constants, u: &Variables, v: &Variables, message_ops: MessageOps) -> bool {
+    pub open spec fn step(c: &Constants, u: &Variables, v: &Variables, message_ops: MessageOps) -> bool {
         &&& u.well_formed(c)
         &&& v.well_formed(c)
         // Only allow receipt of messages that have been sent
