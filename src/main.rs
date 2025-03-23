7b0ef15eec5fb2666bb24c15d1a285280e796139
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-03-23 01:53:39 -0400

    Added proof for the system
    The proof required modification to the system specification that ensures the coordinator always reach the same decision. This is necessary because of the following: -
    
    Consider the situation where `m` hosts out of `n` hosts (`m < n`) have voted `Yes`. Now, the coordinator independently decides to `Abort`. This is a valid safe state because we only have safety restriction for commit decision. Now, over next few transitions, this decision has been relayed to all hosts and the coordinator learns that all `n` hosts have voted `Yes`. In a real system, at this point, the coordinator cannot change the decision from `Abort` to `Commit` or vice versa. That is to say, once a decision has been made, the system cannot re-decide.
    
    Now, we can add a restriction that coordinator can only abort if at least one host votes `No`. This is just a design choice and only represents an ideal case rather than a practical design, where even the coordinator can encounter failure and itself decide to `Abort` even though all hosts voted `Yes`.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,50 @@
 mod distributed_system;
 
+use distributed_system::{
+    decision_message_agrees_with_decision, host, inductive, init, is_valid_transition, next,
+    safety, vote_has_vote_message, vote_message_agrees_with_vote, Constants, Decision, Transition,
+    Variables,
+};
 use vstd::prelude::*;
 
 verus! {
+    pub proof fn ensures_safety(c: &Constants, u: &Variables, v: &Variables)
+    ensures
+        init(c, u) ==> inductive(c, u),
+        inductive(c, u) && next(c, u, v) ==> inductive(c, v),
+        inductive(c, u) ==> safety(c, u)
+    {
+        assert(inductive(c, u) && next(c, u, v) ==> inductive(c, v)) by {
+            assume(inductive(c, u));
+            assume(next(c, u, v));
+            assert(u.well_formed(c));
+
+            assert(v.well_formed(c));
+            assert(vote_message_agrees_with_vote(c, u));
+            assert(decision_message_agrees_with_decision(c, u));
+            assert(vote_has_vote_message(c, u));
+
+            assert(
+                (u.coordinator.decision == Some(Decision::Commit)) ==>
+                (forall |i: int| 0 <= i < v.coordinator.votes.len() ==> v.coordinator.votes[i] == Some(host::Vote::Yes))
+            ) by {
+                assume(u.coordinator.decision == Some(Decision::Commit));
+
+                assert forall |i: int| #![auto]
+                    0 <= i < v.coordinator.votes.len() implies
+                    v.coordinator.votes[i] == Some(host::Vote::Yes)
+                by {
+                    let transition = choose |transition: Transition| is_valid_transition(c, u, v, transition);
+                    match transition {
+                        Transition::CoordinatorStep { message_ops } => {},
+                        Transition::HostStep { host_id, message_ops } => {},
+                    }
+                };
+            };
+
+            assert(forall |i: int| #![auto] 0 <= i < v.hosts.len() && v.hosts[i].decision.is_some() ==> v.hosts[i].decision == v.coordinator.decision);
+        }
+    }
+
     fn main() { }
 }
