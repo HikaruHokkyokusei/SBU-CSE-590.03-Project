6d3797a87922f6ecc8aca77f09124fcbc99a9f78
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-03-25 16:00:10 -0400

    Added `well_formed` and `init` specification for the system

--- src/distributed_system/high_level/mod.rs
+++ src/distributed_system/high_level/mod.rs
@@ -6,4 +6,8 @@ verus! {
     pub struct Variables {
         pub counter: int,
     }
+
+    pub open spec fn init(c: &Constants, u: &Variables) -> bool {
+        &&& u.counter == 0
+    }
 }
--- src/distributed_system/low_level/host.rs
+++ src/distributed_system/low_level/host.rs
@@ -9,4 +9,22 @@ verus! {
         pub holds_counter: bool,
         pub counter: int,
     }
+
+    impl Constants {
+        pub open spec fn well_formed(&self, id: int) -> bool {
+            &&& id >= 0
+            &&& self.id == id
+        }
+    }
+
+    impl Variables {
+        pub open spec fn well_formed(&self, c: &Constants, id: int) -> bool {
+            &&& c.well_formed(id)
+        }
+    }
+
+    pub open spec fn init(c: &Constants, u: &Variables, host_id: int) -> bool {
+        &&& u.well_formed(c, host_id)
+        &&& u.counter == 0
+    }
 }
