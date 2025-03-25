c0323e30da8996a5b1f4ed71b53d7ce7d19992ce
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-03-25 16:00:10 -0400

    Added `well_formed` and `init` specification for the system

--- src/distributed_system/low_level/mod.rs
+++ src/distributed_system/low_level/mod.rs
@@ -23,4 +23,26 @@ verus! {
         pub hosts: Seq<host::Variables>,
         pub network: network::Variables,
     }
+
+    impl Constants {
+        pub open spec fn well_formed(&self) -> bool {
+            &&& self.num_hosts > 0
+            &&& self.hosts.len() == self.num_hosts
+        }
+    }
+
+    impl Variables {
+        pub open spec fn well_formed(&self, c: &Constants) -> bool {
+            &&& c.well_formed()
+            &&& self.hosts.len() == c.hosts.len()
+            &&& forall |idx: int| #![auto] 0 <= idx < self.hosts.len() ==> self.hosts[idx].well_formed(&c.hosts[idx], idx)
+            &&& self.network.well_formed(&c.network)
+        }
+    }
+
+    pub open spec fn init(c: &Constants, u: &Variables) -> bool {
+        &&& u.well_formed(c)
+        &&& forall |idx: int| #![auto] 0 <= idx < u.hosts.len() ==> host::init(&c.hosts[idx], &u.hosts[idx], idx) && if (idx == 0) { u.hosts[idx].holds_counter } else { !u.hosts[idx].holds_counter }
+        &&& network::init(&c.network, &u.network)
+    }
 }
