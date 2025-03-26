b0a0ccfa38612e38af8d05e99a25b27fe7e8180c
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-03-26 00:03:21 -0400

    Added `refinement_init` proof for the system corresponding to `init ==> inductive` clause of `ensures_safety`

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
 mod distributed_system;
 
+use distributed_system::{
+    all_hosts_zero_counter_sum_is_zero, constants_abstraction,
+    high_level::init as high_init,
+    low_level::{
+        inductive, init as low_init, Constants as LowConstants, Variables as LowVariables,
+    },
+    variables_abstraction,
+};
 use vstd::prelude::*;
 
 verus! {
+    // Corresponds to `init(c, u) ==> inductive(c, u)`
+    proof fn refinement_init(c: &LowConstants, u: &LowVariables)
+    requires
+        low_init(c, u),
+    ensures
+        inductive(c, u),
+        high_init(&constants_abstraction(c), &variables_abstraction(c, u)),
+    {
+        let high_constants = constants_abstraction(c);
+        let high_variables = variables_abstraction(c, u);
+        all_hosts_zero_counter_sum_is_zero(u.hosts);
+        assert(high_init(&high_constants, &high_variables));
+    }
+
     fn main() { }
 }
