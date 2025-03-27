e87ee387f08be71561dc9e77e51028b161f7016b
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-03-27 02:34:26 -0400

    Updated the `inductive` specification of the system and the corresponding dependent proofs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use distributed_system::{
     all_hosts_zero_counter_sum_is_zero, constants_abstraction,
     high_level::init as high_init,
     low_level::{
-        inductive, init as low_init, Constants as LowConstants, Variables as LowVariables,
+        inductive, init as low_init, Constants as LowConstants, Message, Variables as LowVariables,
     },
     variables_abstraction,
 };
@@ -19,6 +19,11 @@ verus! {
         inductive(c, u),
         high_init(&constants_abstraction(c), &variables_abstraction(c, u)),
     {
+        assert((forall |i: int| #![auto] 0 <= i < u.hosts.len() ==> !u.hosts[i].holds_counter) ==> (exists |m: Message| u.network.in_flight_messages =~= set![m])) by {
+            assert(u.hosts[0].holds_counter);
+            assert(exists |i: int| #![auto] 0 <= i < u.hosts.len() ==> u.hosts[i].holds_counter);
+        };
+
         let high_constants = constants_abstraction(c);
         let high_variables = variables_abstraction(c, u);
         all_hosts_zero_counter_sum_is_zero(u.hosts);
