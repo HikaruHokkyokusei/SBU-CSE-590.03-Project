fe934c8510a8a3cd93db513414e5bd939a0f8458
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-04-01 21:36:56 -0400

    Added transition specification for the system

--- src/distributed_system/high_level/mod.rs
+++ src/distributed_system/high_level/mod.rs
@@ -1,4 +1,4 @@
-use super::Value;
+use super::{Event, Value};
 use vstd::prelude::*;
 
 verus! {
@@ -10,5 +10,17 @@ verus! {
 
     pub open spec fn init(c: &Constants, u: &Variables) -> bool {
         &&& u.decided_value.is_none()
+    }
+
+    pub open spec fn decide(c: &Constants, u: &Variables, v: &Variables, value: Value) -> bool {
+        &&& u.decided_value.is_some() ==> u.decided_value == Some(value)
+        &&& v.decided_value == Some(value)
+    }
+
+    pub open spec fn next(c: &Constants, u: &Variables, v: &Variables, event: Event) -> bool {
+        match event {
+            Event::Decide { value } => { decide(c, u, v, value) },
+            Event::NoOp => { v == u },
+        }
     }
 }
