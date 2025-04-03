a7616f840efa72ae9bd182324242dc0d0cb80736
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-04-02 23:40:16 -0400

    Added refinement specification for the system that links low level state machine with high level state machine

--- src/distributed_system/mod.rs
+++ src/distributed_system/mod.rs
@@ -1,3 +1,5 @@
+use high_level::{Constants as HighConstants, Variables as HighVariables};
+use low_level::{Constants as LowConstants, Variables as LowVariables};
 use vstd::prelude::*;
 
 verus! {
@@ -9,5 +11,26 @@ verus! {
     pub enum Event {
         Decide { value: Value },
         NoOp,
+    }
+
+    pub open spec fn constants_abstraction(lc: &LowConstants) -> HighConstants
+    recommends
+        lc.well_formed()
+    {
+        HighConstants { }
+    }
+
+    pub open spec fn variables_abstraction(lc: &LowConstants, lv: &LowVariables) -> HighVariables
+    recommends
+        lv.well_formed(lc)
+    {
+        HighVariables {
+            decided_value: if (exists |i: int| #![auto] 0 <= i < lv.hosts.len() && lv.hosts[i].decide_value.is_some()) {
+                let i = choose |i: int| #![auto] 0 <= i < lv.hosts.len() && lv.hosts[i].decide_value.is_some();
+                lv.hosts[i].decide_value
+            } else {
+                None
+            }
+        }
     }
 }
