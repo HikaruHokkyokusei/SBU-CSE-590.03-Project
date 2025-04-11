48a2083802ea2c2f3de07464ea8cec162af69193
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-04-11 01:07:27 -0400

    Added `if_accepted_then_all_future_promise_have_some_accepted_value` and `if_system_accepted_exists_some_accept_value_in_future_promise_quorum` specifications to `inductive` specification requirement

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,15 @@ use distributed_system::{
         decide_message_exist_only_if_system_accepted_on_corresponding_ballot,
         if_accepted_message_exists_then_accept_message_exists,
         if_accepted_message_exists_then_sender_has_accepted_some_value,
+        if_accepted_then_all_future_promise_have_some_accepted_value,
         if_host_promised_or_accepted_has_ballot_then_network_contains_corresponding_prepare,
         if_promise_message_exists_then_sender_has_promised,
-        if_someone_has_accepted_then_someone_has_proposed, inductive,
+        if_someone_has_accepted_then_someone_has_proposed,
+        if_system_accepted_exists_some_accept_value_in_future_promise_quorum, inductive,
         inductive_next_implies_all_promised_and_accepted_sets_of_all_hosts_are_finite,
         inductive_next_implies_decide_has_decide_message_in_network,
         inductive_next_implies_if_accepted_message_exists_then_accept_message_exists,
+        inductive_next_implies_if_system_accepted_exists_some_accept_value_in_future_promise_quorum,
         init as low_init, next as low_next, promise_has_prepare_message_in_network,
         promised_has_promise_message_in_network, safety, Constants as LowConstants,
         Variables as LowVariables,
@@ -64,6 +67,8 @@ verus! {
             assert(if_accepted_message_exists_then_sender_has_accepted_some_value(c, v));
             assert(if_accepted_message_exists_then_accept_message_exists(c, v)) by { inductive_next_implies_if_accepted_message_exists_then_accept_message_exists(c, u, v, event); };
             assert(if_someone_has_accepted_then_someone_has_proposed(c, v));
+            assert(if_accepted_then_all_future_promise_have_some_accepted_value(c, v));
+            assert(if_system_accepted_exists_some_accept_value_in_future_promise_quorum(c, v)) by { inductive_next_implies_if_system_accepted_exists_some_accept_value_in_future_promise_quorum(c, u, v, event); };
             assert(decide_message_exist_only_if_system_accepted_on_corresponding_ballot(c, v));
             assert(decide_has_decide_message_in_network(c, v)) by { inductive_next_implies_decide_has_decide_message_in_network(c, u, v, event); };
             assert(all_decide_messages_hold_same_value(c, v)) by { assume(false); };
