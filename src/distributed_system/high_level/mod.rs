3633aa2655b85107a7fc7f05b8050867018f1e42
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-04-20 19:07:58 -0400

    Updated high level state machine to enable Multi-Paxos

--- src/distributed_system/high_level/mod.rs
+++ src/distributed_system/high_level/mod.rs
@@ -5,21 +5,21 @@ verus! {
     pub struct Constants {}
 
     pub struct Variables {
-        pub decided_value: Option<Value>,
+        pub decided_value: Map<nat, Value>,
     }
 
     pub open spec fn init(c: &Constants, u: &Variables) -> bool {
-        &&& u.decided_value.is_none()
+        &&& u.decided_value.is_empty()
     }
 
-    pub open spec fn decide(c: &Constants, u: &Variables, v: &Variables, value: Value) -> bool {
-        &&& u.decided_value.is_some() ==> u.decided_value == Some(value)
-        &&& v.decided_value == Some(value)
+    pub open spec fn decide(c: &Constants, u: &Variables, v: &Variables, key: nat, value: Value) -> bool {
+        &&& u.decided_value.contains_key(key) ==> u.decided_value[key] == value
+        &&& v.decided_value == u.decided_value.insert(key, value)
     }
 
     pub open spec fn next(c: &Constants, u: &Variables, v: &Variables, event: Event) -> bool {
         match event {
-            Event::Decide { value } => { decide(c, u, v, value) },
+            Event::Decide { key, value } => { decide(c, u, v, key, value) },
             Event::NoOp => { v == u },
         }
     }
