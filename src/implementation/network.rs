7ef5689c1d87aa098b5640f40ba58a82a9b6a294
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2025-05-14 22:26:49 -0400

    Added step transition for network in implementation

--- src/implementation/network.rs
+++ src/implementation/network.rs
@@ -1,10 +1,12 @@
-use super::Message;
-use crate::distributed_system::low_level::{
-    network::{Constants as LowConstants, Variables as LowVariables},
-    Message as LowMessage,
+use super::{Message, NetworkOperation};
+use crate::{
+    axiom_message_obeys_hash_table_key_model,
+    distributed_system::low_level::{
+        network::{step as low_step, Constants as LowConstants, Variables as LowVariables},
+        Message as LowMessage,
+    },
 };
 use std::collections::HashSet;
-
 use vstd::prelude::*;
 
 verus! {
@@ -31,7 +33,7 @@ verus! {
 
         pub open spec fn into_spec(&self) -> LowVariables {
             LowVariables {
-                in_flight_messages: Set::new(|message: LowMessage| self.in_flight_messages@.contains(Message::from_spec(message))),
+                in_flight_messages: Set::new(|message: LowMessage| Message::valid_spec(message) && self.in_flight_messages@.contains(Message::from_spec(message))),
             }
         }
     }
@@ -60,6 +62,34 @@ verus! {
         {
             Self {
                 in_flight_messages: HashSet::new(),
+            }
+        }
+
+        pub exec fn fill_in_flight_messages(&mut self, msg: Message)
+        ensures ({
+            let (old_spec, new_spec) = (old(self).into_spec(), self.into_spec());
+
+            &&& new_spec =~= LowVariables { in_flight_messages: old_spec.in_flight_messages.insert(msg.into_spec()) }
+            &&& self.in_flight_messages@ == old(self).in_flight_messages@.insert(msg)
+        })
+        {
+            self.in_flight_messages.insert(msg);
+            proof! {
+                axiom_message_obeys_hash_table_key_model();
+                let (old_spec, new_spec) = (old(self).into_spec(), self.into_spec());
+                assert(new_spec.in_flight_messages =~= old_spec.in_flight_messages.insert(msg.into_spec()));
+            };
+        }
+
+        pub exec fn step(&mut self, c: &Constants, net_op: &NetworkOperation)
+        requires
+            net_op.recv.is_some() ==> old(self).in_flight_messages@.contains(net_op.recv.unwrap()),
+        ensures
+            low_step(&c.into_spec(), &old(self).into_spec(), &self.into_spec(), net_op.into_spec()),
+            net_op.send.is_some() ==> self.in_flight_messages@ =~= old(self).in_flight_messages@.insert(net_op.send.unwrap()),
+        {
+            if let Some(send) = net_op.send.as_ref() {
+                self.fill_in_flight_messages(send.clone());
             }
         }
     }
